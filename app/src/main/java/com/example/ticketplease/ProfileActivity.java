107fc1aa86e826f619c675700c52c952366b85c1
patryk-l <patryk.luczak1@unilodz.eu>
2020-12-06 20:41:59 +0100

    generate watched films in loop

--- app/src/main/java/com/example/ticketplease/ProfileActivity.java
+++ app/src/main/java/com/example/ticketplease/ProfileActivity.java
@@ -3,11 +3,18 @@ package com.example.ticketplease;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+
 public class ProfileActivity extends AppCompatActivity {
+    ArrayList<ProfileFilmListItem> filmsArray = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +43,26 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(new Intent(ProfileActivity.this, HomeActivity.class));
             }
         });
+        /*ListView films;
+        String[] Titles;
+        films=findViewById(R.id.ListFilms);
+        Titles = new DateFormatSymbols().getMonths();
+        ArrayAdapter<String> titlesArray = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,Titles);
+        films.setAdapter(titlesArray);*/
+        getFilms();
+        ListView films;
+        ListAdapter listAdapter;
+        films=findViewById(R.id.ListFilms);
+        listAdapter = new ProfileListView(this,filmsArray);
+        films.setAdapter(listAdapter);
+    }
+
+    private void getFilms() {
+        filmsArray.add(new ProfileFilmListItem("Czarny ekran","Film bez wizji",R.drawable.small_poster));
+        filmsArray.add(new ProfileFilmListItem("Czarny ekran 2","Film bez wizji. Kolejna część oscarowej produkcji",R.drawable.small_poster));
+        filmsArray.add(new ProfileFilmListItem("Czarny ekran 3","Film bez wizji. Niektórzy myślą, że to cały czas pierwsza część",R.drawable.small_poster));
+        filmsArray.add(new ProfileFilmListItem("Czarny ekran 4","Film bez wizji. Niektórzy myślą, że to cały czas pierwsza część",R.drawable.small_poster));
+        filmsArray.add(new ProfileFilmListItem("Czarny ekran 5","Film bez wizji. Niektórzy myślą, że to cały czas pierwsza część",R.drawable.small_poster));
+        filmsArray.add(new ProfileFilmListItem("Czarny ekran 6","Film bez wizji. Niektórzy myślą, że to cały czas pierwsza część",R.drawable.small_poster));
     }
 }
---
+++ app/src/main/java/com/example/ticketplease/ProfileFilmListItem.java
@@ -0,0 +1,12 @@
+package com.example.ticketplease;
+
+public class ProfileFilmListItem {
+    String Title;
+    String Description;
+    int Poster;
+    ProfileFilmListItem(String T, String D, int P){
+        Title=T;
+        Description=D;
+        Poster=P;
+    }
+}
