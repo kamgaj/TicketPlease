0e2d6afc2b98b88a70d460ab452ae8a3e856df0e
patryk-l <patryk.luczak1@unilodz.eu>
2020-12-06 18:09:31 +0100

    progress in ui/ux

--- app/src/main/java/com/example/ticketplease/SearchActivity.java
+++ app/src/main/java/com/example/ticketplease/SearchActivity.java
@@ -3,11 +3,25 @@ package com.example.ticketplease;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.widget.NestedScrollView;
+
+import com.google.android.material.resources.TextAppearance;
+
+import java.util.ArrayList;
+
+import io.opencensus.resource.Resource;
 
 public class SearchActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +50,21 @@ public class SearchActivity extends AppCompatActivity {
                 startActivity(new Intent(SearchActivity.this, ProfileActivity.class));
             }
         });
+        final Button[] buttons = new Button[15];
+        LinearLayout container = (LinearLayout) findViewById(R.id.scrollResult);
+
+       LinearLayout .LayoutParams test = new LinearLayout .LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (int i = 0; i < 15; i++) {
+            buttons[i] = new Button(this);
+            //i don't think you should do that
+            //myTextViews[i].setId(R.id.testid+i);
+            buttons[i].setText("Film "+(i+1));
+            buttons[i].setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            buttons[i].setTextSize(16);
+            buttons[i].setLayoutParams(test);
+            container.addView(buttons[i]);
+        }
     }
 }
