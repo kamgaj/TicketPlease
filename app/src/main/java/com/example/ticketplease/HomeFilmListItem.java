7371c009a924c18d954f0149f545081d500f8797
patryk-l <patryk.luczak1@unilodz.eu>
2020-12-12 22:22:11 +0100

    generate items in homepage in loop

---
+++ app/src/main/java/com/example/ticketplease/HomeFilmListItem.java
@@ -0,0 +1,12 @@
+package com.example.ticketplease;
+
+public class HomeFilmListItem {
+    String Title;
+    Double Price;
+    int Poster;
+    HomeFilmListItem(String T, Double Pr, int P){
+        Title=T;
+        Price=Pr;
+        Poster=P;
+    }
+}
