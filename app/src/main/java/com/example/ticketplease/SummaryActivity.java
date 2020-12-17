a354ef692dac99b7d9256f2434440feff8d7442e
patryk-l <patryk.luczak1@unilodz.eu>
2020-12-17 15:52:49 +0100

    Improve booking tickets page

--- app/src/main/java/com/example/ticketplease/SummaryActivity.java
+++ app/src/main/java/com/example/ticketplease/SummaryActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,16 @@ public class SummaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.summary_page);
+        String numberOfTickets = getIntent().getStringExtra("Tickets");
+        TextView text = findViewById(R.id.NumberOfTickets);
+
+        if(numberOfTickets.equals("0")) {
+            text.setText("Kupiłeś 0 biletów");
+        } else if(numberOfTickets.equals("1")){
+            text.setText("Kupiłeś 1 bilet");
+        }else  {
+            text.setText("Kupiłeś "+ numberOfTickets + " bilety");
+        }
 
         Button ok;
         ok = findViewById(R.id.chip6);
