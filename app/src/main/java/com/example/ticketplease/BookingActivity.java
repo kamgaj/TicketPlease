dd90a08c40bd3fa62a5e33b2fd6102469ffbb4b5
patryk-l <patryk.luczak1@unilodz.eu>
2020-12-21 02:58:27 +0100

    add option to choose cinema,technology and date

--- app/src/main/java/com/example/ticketplease/BookingActivity.java
+++ app/src/main/java/com/example/ticketplease/BookingActivity.java
@@ -1,22 +1,35 @@
 package com.example.ticketplease;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.Sampler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.chip.Chip;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class BookingActivity  extends AppCompatActivity {
     ArrayList<Integer> alreadyBooked= new ArrayList<>();
     ArrayList<String> userChoice = new ArrayList<String>();
+    List<String> cinema=new ArrayList<>();
+    List<String> technology=new ArrayList<>();
+    Calendar calendar=Calendar.getInstance();
+    String readyDate=String.valueOf(calendar.get(calendar.DAY_OF_MONTH))+"."+String.valueOf(calendar.get(calendar.MONTH))+"."+String.valueOf(calendar.get(calendar.YEAR));
     int tickets=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +44,70 @@ public class BookingActivity  extends AppCompatActivity {
 
                 Intent intent = new Intent(BookingActivity.this, SummaryActivity.class);
                 intent.putExtra("Tickets", String.valueOf(tickets));
+                intent.putExtra("Date", readyDate);
                 startActivity(intent);
             }
         });
         AddBookedBefore();
         addButtons();
+        Chip date = findViewById(R.id.dateFilm);
+        date.setHint(readyDate);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int day=calendar.get(Calendar.DAY_OF_MONTH);
+                int month=calendar.get(Calendar.MONTH);
+                int year=calendar.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog=new DatePickerDialog(BookingActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        calendar.set(i,i1,i2);
+                        readyDate = String.valueOf(i2)+"."+String.valueOf(i1)+"."+String.valueOf(i);
+                        date.setHint(readyDate);
+                    }
+                }, day,month,year);
+                datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
+                datePickerDialog.updateDate(year,month,day);
+
+                datePickerDialog.show();
+            }
+        });
+        addCinema();
+        Chip Cinema = findViewById(R.id.cinema);
+        Cinema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] t=cinema.toArray(new String[0]);
+                AlertDialog.Builder builder=new AlertDialog.Builder(BookingActivity.this);
+                builder.setTitle("Wybierz kino");
+                builder.setSingleChoiceItems(t, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Cinema.setHint(t[i]);
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
+        addTechnology();
+        Chip Tech = findViewById(R.id.technology);
+        Tech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] t=technology.toArray(new String[0]);
+                AlertDialog.Builder builder=new AlertDialog.Builder(BookingActivity.this);
+                builder.setTitle("Wybierz technologie");
+                builder.setSingleChoiceItems(t, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Tech.setHint(t[i]);
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
 
     }
     void addButtons(){
@@ -88,5 +160,17 @@ public class BookingActivity  extends AppCompatActivity {
         alreadyBooked.add(5);
         alreadyBooked.add(6);
         alreadyBooked.add(20);
+    }
+    void addCinema(){
+        cinema.add("Kino 1");
+        cinema.add("Kino 2");
+        cinema.add("Kino 3");
+        cinema.add("Kino 4");
+    }
+    void addTechnology(){
+        technology.add("2D");
+        technology.add("3D");
+        technology.add("4D");
+        technology.add("5D");
     }
 }
--- app/src/main/java/com/example/ticketplease/SummaryActivity.java
+++ app/src/main/java/com/example/ticketplease/SummaryActivity.java
@@ -14,6 +14,7 @@ public class SummaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.summary_page);
         String numberOfTickets = getIntent().getStringExtra("Tickets");
+        String dateTicket = getIntent().getStringExtra("Date");
         TextView text = findViewById(R.id.NumberOfTickets);
 
         if(numberOfTickets.equals("2")||numberOfTickets.equals("3")||numberOfTickets.equals("4")) {
@@ -23,7 +24,8 @@ public class SummaryActivity extends AppCompatActivity {
         }else  {
             text.setText("Kupiłeś "+ numberOfTickets +" biletów");
         }
-
+        TextView textWithDate=findViewById(R.id.date);
+        textWithDate.setText("Data seansu: "+dateTicket);
         Button ok;
         ok = findViewById(R.id.chip6);
         ok.setOnClickListener(new View.OnClickListener() {
