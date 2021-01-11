22d02305a42a7a98d1bf3febf0e015a48f30e5c4
kduczek <duczek.krystian@gmail.com>
2021-01-11 10:03:46 +0100

    Added expandable Description in DescriptionActivity

--- app/src/main/java/com/example/ticketplease/DescriptionActivity.java
+++ app/src/main/java/com/example/ticketplease/DescriptionActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,22 +32,25 @@ import java.util.List;
 import java.util.Objects;
 
 public class DescriptionActivity extends AppCompatActivity {
-
+    TextView title;
+    TextView description;
+    TextView genres;
     int x=0;
     StorageReference ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.description);
-        TextView title = findViewById(R.id.titleText);
+        title = findViewById(R.id.titleText);
         String titleFromIntent = getIntent().getStringExtra("Movie_title");
         x = getIntent().getIntExtra("From_where",0);
         title.setText(titleFromIntent);
-        TextView description = findViewById(R.id.descriptionText);
-        TextView genres = findViewById(R.id.genreText);
+        description = findViewById(R.id.descriptionText);
+        genres = findViewById(R.id.genreText);
         ImageView moviePoster = findViewById(R.id.filmPoster);
 
         getMovieFromFirebase(titleFromIntent, description, genres, moviePoster);
+
 
         ImageView BuyTicket;
         BuyTicket = (ImageView) findViewById(R.id.BuyTicketButton);
@@ -69,6 +78,7 @@ public class DescriptionActivity extends AppCompatActivity {
                         if(task.isSuccessful()) {
                             for(QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
                                 tvDesc.setText(document.getString("Description"));
+                                makeTextViewResizable(tvDesc, 4, "Zobacz więcej", true);
                                 genresList.addAll((Collection<? extends String>) document.get("Genres"));
 
                                 String path = document.getString("Poster_link");
@@ -94,6 +104,64 @@ public class DescriptionActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    public static void makeTextViewResizable(final TextView tv, final int maxLine, final String expandText, final boolean viewMore) {
+
+        if (tv.getTag() == null) {
+            tv.setTag(tv.getText());
+        }
+        ViewTreeObserver vto = tv.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onGlobalLayout() {
+                String text;
+                int lineEndIndex;
+                ViewTreeObserver obs = tv.getViewTreeObserver();
+                ((ViewTreeObserver) obs).removeGlobalOnLayoutListener(this);
+                if (maxLine == 0) {
+                    lineEndIndex = tv.getLayout().getLineEnd(0);
+                    text = tv.getText().subSequence(0, lineEndIndex - expandText.length() + 1) + " " + expandText;
+                } else if (maxLine > 0 && tv.getLineCount() >= maxLine) {
+                    lineEndIndex = tv.getLayout().getLineEnd(maxLine - 1);
+                    text = tv.getText().subSequence(0, lineEndIndex - expandText.length() + 1) + " " + expandText;
+                } else {
+                    lineEndIndex = tv.getLayout().getLineEnd(tv.getLayout().getLineCount() - 1);
+                    text = tv.getText().subSequence(0, lineEndIndex) + " " + expandText;
+                }
+                tv.setText(text);
+                tv.setMovementMethod(LinkMovementMethod.getInstance());
+                tv.setText(
+                        addClickablePartTextViewResizable(Html.fromHtml(tv.getText().toString()), tv, lineEndIndex, expandText,
+                                viewMore), TextView.BufferType.SPANNABLE);
+            }
+        });
+    }
+
+    private static SpannableStringBuilder addClickablePartTextViewResizable(final Spanned strSpanned, final TextView tv,
+                                                                            final int maxLine, final String spanableText, final boolean viewMore) {
+        String str = strSpanned.toString();
+        SpannableStringBuilder ssb = new SpannableStringBuilder(strSpanned);
+
+        if (str.contains(spanableText)) {
+            ssb.setSpan(new MySpannable(false){
+                @Override
+                public void onClick(View widget) {
+                    tv.setLayoutParams(tv.getLayoutParams());
+                    tv.setText(tv.getTag().toString(), TextView.BufferType.SPANNABLE);
+                    tv.invalidate();
+                    if (viewMore) {
+                        makeTextViewResizable(tv, -1, "Zobacz mniej", false);
+                    } else {
+                        makeTextViewResizable(tv, 4, "Zobacz więcej", true);
+                    }
+                }
+            }, str.indexOf(spanableText), str.indexOf(spanableText) + spanableText.length(), 0);
+
+        }
+        return ssb;
     }
 
 }
