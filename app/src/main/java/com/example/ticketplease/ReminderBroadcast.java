2dd42615ba20425c8e267f9c1bf9c383e1a07d65
kduczek <duczek.krystian@gmail.com>
2021-01-19 11:54:12 +0100

    Custom notification created but without movie thumbnail

--- app/src/main/java/com/example/ticketplease/ReminderBroadcast.java
+++ app/src/main/java/com/example/ticketplease/ReminderBroadcast.java
@@ -1,9 +1,9 @@
 package com.example.ticketplease;
 
-import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -11,10 +11,13 @@ import androidx.core.app.NotificationManagerCompat;
 public class ReminderBroadcast extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        RemoteViews remoteViews = new RemoteViews(BuildConfig.APPLICATION_ID,  R.layout.custom_notification);
+        remoteViews.setTextViewText(R.id.content_title, "Movie Title");
+        remoteViews.setTextViewText(R.id.content_text, "Masz seans za 2 godziny. Ruszaj dupe xD");
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "reminder")
+                .setCustomContentView(remoteViews)
                 .setSmallIcon(R.drawable.buy_ticket)
-                .setContentTitle("Movie Title")
-                .setContentText("Masz seans za 2 godziny. Ruszaj dupe xD")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
