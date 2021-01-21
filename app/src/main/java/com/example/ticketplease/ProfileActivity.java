00af61cc199802dadd3c1837ca35d0e8a4e906c9
patryk-l <patryk.luczak1@unilodz.eu>
2021-01-21 20:07:40 +0100

    fix problem with loading list

--- app/src/main/java/com/example/ticketplease/ProfileActivity.java
+++ app/src/main/java/com/example/ticketplease/ProfileActivity.java
@@ -224,8 +224,10 @@ public class ProfileActivity extends AppCompatActivity {
                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            int counter=0;
                             if(task.isSuccessful()) {
                                 for(QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                    counter++;
                                     String tempDate = document.getString("date");
                                     try {
                                         Date queryDate = formatter.parse(Objects.requireNonNull(tempDate));
@@ -267,7 +269,10 @@ public class ProfileActivity extends AppCompatActivity {
                                 }
 
 
-
+                                if(counter==0){
+                                    filmsArray=new ArrayList<>();
+                                    printWatched(0);
+                                }
                             } else {
                                 Log.d(TAG, "Watched films, Bookings Collection Query FAILS");
                             }
@@ -294,7 +299,9 @@ public class ProfileActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
                             if(task.isSuccessful()) {
+                                int counter=0;
                                 for(QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                    counter++;
                                     String dateSource = document.getString("date");
                                     String time = document.getString("time");
                                     List<String> items = Arrays.asList(dateSource.split("\\."));
@@ -349,6 +356,10 @@ public class ProfileActivity extends AppCompatActivity {
                                                 printWatched(1);
                                             }
                                         }
+                                }
+                                if(counter==0){
+                                    filmsArray=new ArrayList<ProfileFilmListItem>();
+                                    printWatched(0);
                                 }
                             } else {
                                 Log.d(TAG, "Watched films, Bookings Collection Query FAILS");
