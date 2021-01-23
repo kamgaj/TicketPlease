7ca29ea1c8910c79e8ab4d622e2e9a965409d482
patryk-l <patryk.luczak1@unilodz.eu>
2021-01-23 01:04:57 +0100

    cleanup in profile

--- app/src/main/java/com/example/ticketplease/ProfileCustomListView.java
+++ app/src/main/java/com/example/ticketplease/ProfileCustomListView.java
@@ -5,8 +5,25 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ProfileCustomListView {
-    TextView title ;
-    ImageView poster;
-    TextView description;
-    LinearLayout llMainRow;
+    private TextView title ;
+    private ImageView poster;
+    private TextView description;
+
+    public ProfileCustomListView(TextView title, ImageView poster, TextView description) {
+        this.title = title;
+        this.poster = poster;
+        this.description = description;
+    }
+
+    public TextView getTitle() {
+        return title;
+    }
+
+    public ImageView getPoster() {
+        return poster;
+    }
+
+    public TextView getDescription() {
+        return description;
+    }
 }
--- app/src/main/java/com/example/ticketplease/ProfileFilmListItem.java
+++ app/src/main/java/com/example/ticketplease/ProfileFilmListItem.java
@@ -1,15 +1,15 @@
 package com.example.ticketplease;
 
 public class ProfileFilmListItem {
-    String Title;
-    String Description;
-    String Poster;
-    String Id;
-    String Date;
-    String Time;
-    String Seats;
-    String Cinema;
-    String numberTickets;
+    private String Title;
+    private String Description;
+    private String Poster;
+    private String Id;
+    private String Date;
+    private String Time;
+    private String Seats;
+    private String Cinema;
+    private String numberTickets;
     ProfileFilmListItem(String T, String D, String P, String I,String Date, String Time,String Cinema, String numberTickets, String Seats){
         Title=T;
         Description=D;
@@ -28,5 +28,33 @@ public class ProfileFilmListItem {
 
     public String getTime() {
         return Time;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getPoster() {
+        return Poster;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getSeats() {
+        return Seats;
+    }
+
+    public String getCinema() {
+        return Cinema;
+    }
+
+    public String getNumberTickets() {
+        return numberTickets;
     }
 }
